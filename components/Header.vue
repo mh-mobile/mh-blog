d2cf10ac95faa1725a39be6b57171290302cc5e1
mh-mobile <mh.mobiler@gmail.com>
2020-12-19 21:54:54 +0900

    記事のリンクを修正

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,7 @@
 <template>
   <header class="bg-white py-4 px-4 md:px-20">
     <div class="inner flex justify-between mx-auto">
-      <nuxt-link to="/"><div class="logo">Blog</div></nuxt-link>
-      <nav>リンクとか</nav>
+      <nuxt-link to="/"><div class="logo">mh-blog</div></nuxt-link>
     </div>
   </header>
 </template>
